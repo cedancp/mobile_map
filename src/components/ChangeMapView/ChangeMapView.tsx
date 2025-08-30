9a285effc1f01032c7e51e8a670c8568cd58e76c
Cesar Cobo <cesarcobo@Cesars-MacBook-Pro.local>
2025-08-30 12:12:18 +0200

    feat: poup for information

--- src/components/ChangeMapView/ChangeMapView.tsx
+++ src/components/ChangeMapView/ChangeMapView.tsx
@@ -1,8 +1,9 @@
-import { LatLng, polyline, layerGroup, LayerGroup } from "leaflet";
+import { LatLng, polyline, layerGroup, LayerGroup, Tooltip } from "leaflet";
 import React, { useEffect, useRef } from "react";
 import { useMap } from "react-leaflet";
 import { SensorData } from "../../types/SensorData";
 import { getColor } from "./changeMapViewUtils";
+import ReactDOMServer from "react-dom/server";
 
 export type ChangeMapViewProps = {
   positions: SensorData[];
@@ -33,7 +34,48 @@ const ChangeMapView: React.FC<ChangeMapViewProps> = ({ positions }) => {
 
         const newPolyline = polyline(latLngPos, {
           color: getColor(sensorData.P25),
+          weight: 5,
         });
+
+        const TooltipContent = () => (
+          <div
+            style={{
+              border: `1px solid ${getColor(sensorData.P25)}`,
+              borderRadius: "8px",
+              overflow: "hidden",
+            }}
+          >
+            <div
+              style={{
+                backgroundColor: getColor(sensorData.P25),
+                color: "white",
+                padding: "5px",
+                fontWeight: "bold",
+              }}
+            >
+              Sensor Information
+            </div>
+            <div style={{ padding: "5px" }}>
+              <div>
+                <strong>P25:</strong> {sensorData.P25}
+              </div>
+              <div>
+                <strong>P10:</strong> {sensorData.P10}
+              </div>
+              <div>
+                <strong>Spd:</strong> {sensorData.spd}
+              </div>
+            </div>
+          </div>
+        );
+
+        const tooltip = new Tooltip({
+          offset: [0, 10],
+          opacity: 0.9,
+        }).setContent(
+          ReactDOMServer.renderToString(<TooltipContent />)
+        );
+        newPolyline.bindTooltip(tooltip);
 
         pathsLayerRef.current.addLayer(newPolyline).addTo(map);
       } else {
